e730498f785e20c5ddeacd51c4a42104b004f7ff
Richard Leshik <richardleshik@gmail.com>
2024-04-28 20:26:35 +0300

    feat: add Main page

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,9 @@
+import styles from './Main.module.scss'
+
+export const Main = () => {
+	return (
+		<main className={styles.main}>
+			Main
+		</main>
+	)
+}
---
+++ src/pages/main/index.ts
@@ -0,0 +1,2 @@
+export * from './Main.tsx'
+export * from './Main.module.scss'
