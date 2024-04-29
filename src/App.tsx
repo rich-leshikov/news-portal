d4237997a1127716c7655e58af5f5cc49cc6bf7e
Richard Leshik <richardleshik@gmail.com>
2024-04-29 21:47:54 +0300

    feat: update Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {Header} from './components'
+import {Main} from './pages'
 
 export const App = () => {
 	return (
 		<>
 			<Header/>
+			<div className="container">
+				<Main/>
+			</div>
 		</>
 	)
 }
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,27 @@
+import {useEffect, useState} from 'react'
+import {getNews} from '../../api'
+import {NewsBanner} from '../../components'
+
 import styles from './Main.module.scss'
 
 export const Main = () => {
+	const [news, setNews] = useState([])
+
+	useEffect(() => {
+		const fetchNews = async () => {
+			try {
+				const response = await getNews()
+				setNews(response.news)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchNews()
+	}, [])
+
 	return (
 		<main className={styles.main}>
-			Main
+			{news.length > 0 && <NewsBanner item={news[0]}/>}
 		</main>
 	)
 }
