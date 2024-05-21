2268f049a617906164006676d202acaaec3bea36
Richard Leshik <richardleshik@gmail.com>
2024-05-21 23:51:37 +0300

    feat: update useFetch types

--- src/shared/hooks/useFetch.ts
+++ src/shared/hooks/useFetch.ts
@@ -1,9 +1,19 @@
 import {useEffect, useState} from 'react'
 
-export const useFetch = (fetchFunction, params?) => {
-	const [data, setData] = useState<unknown | null>(null)
-	const [isLoading, setIsLoading] = useState(true)
-	const [error, setError] = useState<unknown | null>(null)
+type FetchFunction<P, T> = {
+	(params?: P): Promise<T>
+}
+
+type UseFetchResult<T> = {
+	data: T | null | undefined
+	isLoading: boolean
+	error: Error | null
+}
+
+export const useFetch = <T, P>(fetchFunction: FetchFunction<P, T>, params?: P): UseFetchResult<T> => {
+	const [data, setData] = useState<T | null>(null)
+	const [isLoading, setIsLoading] = useState<boolean>(true)
+	const [error, setError] = useState<Error | null>(null)
 
 	const stringParams = params ? new URLSearchParams(params).toString() : ''
 
@@ -14,7 +24,7 @@ export const useFetch = (fetchFunction, params?) => {
 				const result = await fetchFunction(params)
 				setData(result)
 			} catch (error) {
-				setError(error)
+				setError(error as Error)
 			} finally {
 				setIsLoading(false)
 			}
