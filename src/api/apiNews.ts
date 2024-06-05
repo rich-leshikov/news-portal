2384dbe962bb7481bc2f5e2f79d503ff25f26ca0
Richard Leshik <richardleshik@gmail.com>
2024-06-05 22:29:34 +0300

    feat: update apiNews typing

--- src/api/apiNews.ts
+++ src/api/apiNews.ts
@@ -1,12 +1,14 @@
 import axios from 'axios'
-import {TFilters} from '../shared'
+import {TCategoriesApiResponse, TNewsApiResponse, TParams} from '../shared'
 
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async ({page_number = 1, page_size = 10, category, keywords}: TFilters) => {
+export const getNews = async (params?: TParams): Promise<TNewsApiResponse> => {
 	try {
-		const response = await axios.get(`${BASE_URL}search`, {
+		const {page_number = 1, page_size = 10, category, keywords} = params || {}
+
+		const response = await axios.get<TNewsApiResponse>(`${BASE_URL}search`, {
 			params: {
 				apiKey: API_KEY,
 				page_number,
@@ -19,12 +21,13 @@ export const getNews = async ({page_number = 1, page_size = 10, category, keywor
 		return response.data
 	} catch (error) {
 		console.log(error)
+		return {news: [], page: 1, status: 'error'}
 	}
 }
 
-export const getLatestNews = async () => {
+export const getLatestNews = async (): Promise<TNewsApiResponse> => {
 	try {
-		const response = await axios.get(`${BASE_URL}latest-news`, {
+		const response = await axios.get<TNewsApiResponse>(`${BASE_URL}latest-news`, {
 			params: {
 				apiKey: API_KEY
 			}
@@ -33,12 +36,13 @@ export const getLatestNews = async () => {
 		return response.data
 	} catch (error) {
 		console.log(error)
+		return {news: [], page: 1, status: 'error'}
 	}
 }
 
-export const getCategories = async () => {
+export const getCategories = async (): Promise<TCategoriesApiResponse> => {
 	try {
-		const response = await axios.get(`${BASE_URL}available/categories`, {
+		const response = await axios.get<TCategoriesApiResponse>(`${BASE_URL}available/categories`, {
 			params: {
 				apiKey: API_KEY
 			}
@@ -47,5 +51,6 @@ export const getCategories = async () => {
 		return response.data
 	} catch (error) {
 		console.log(error)
+		return {categories: [], description: '', status: 'error'}
 	}
 }
