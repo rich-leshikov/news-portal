bee81bab3a9f4707ca881b055dcb3b3004614df5
Richard Leshik <richardleshik@gmail.com>
2024-06-17 21:35:46 +0300

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import {Header} from './components'
 import {Main} from './pages'
+import {useState} from 'react'
 
 export const App = () => {
+	const [isDark, setIsDark] = useState(true)
+
 	return (
-		<>
-			<Header/>
+		<div className={`app ${isDark ? 'dark' : 'light'}`}>
+			<Header isDark={isDark}/>
 			<div className="container">
 				<Main/>
 			</div>
-		</>
+		</div>
 	)
 }
