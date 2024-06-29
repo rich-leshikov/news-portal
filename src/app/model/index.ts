4968b7c96088b15760a5089f9cc969c31b8b5e10
Richard Leshik <richardleshik@gmail.com>
2024-06-29 20:11:30 +0300

    feat: add newsApi

--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -1,1 +1,3 @@
+export * from './services'
+export * from './slices'
 export * from './store.ts'
---
+++ src/app/model/services/newsApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {TNewsApiResponse, TParams} from '../../../shared'
+
+const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY
+
+export const newsApi = createApi({
+	reducerPath: 'newsApi',
+	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+	endpoints: (builder) => ({
+		getNews: builder.query<TNewsApiResponse, TParams>({
+			query: (params) => {
+				const {page_number = 1, page_size = 10, category, keywords} = params || {}
+
+				return {
+					url: 'search',
+					params: {
+						apiKey: API_KEY,
+						page_number,
+						page_size,
+						category,
+						keywords
+					}
+				}
+			},
+		}),
+	}),
+})
+
+export const { useGetNewsQuery } = newsApi
