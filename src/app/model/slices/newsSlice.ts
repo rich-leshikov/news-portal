21ef0dea9220c3489470509b265a4baa97c5e447
Richard Leshik <richardleshik@gmail.com>
2024-06-29 23:03:03 +0300

    feat: add setFilters to newsSlice

--- src/app/model/slices/newsSlice.ts
+++ src/app/model/slices/newsSlice.ts
@@ -1,13 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import {TNewsItem} from '../../../shared'
+import {createSlice} from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
+import {TFilters, TNewsItem} from '../../../shared'
+import {PAGE_SIZE} from '../../../constants'
 
 type TState = {
-	news: TNewsItem[]
+	news: TNewsItem[],
+	filters: TFilters
 }
 
 const initialState: TState = {
-	news: []
+	news: [],
+	filters: {
+		page_number: 1,
+		page_size: PAGE_SIZE,
+		category: null,
+		keywords: ''
+	}
 }
 
 export const newsSlice = createSlice({
@@ -17,9 +25,13 @@ export const newsSlice = createSlice({
 		setNews: (state, action: PayloadAction<TNewsItem[]>) => {
 			state.news = action.payload
 		},
+		setFilters: (state, action: PayloadAction<{ key: string, value: number | string | null }>) => {
+			const {key, value} = action.payload
+			state.filters = {...state.filters, [key]: value}
+		},
 	},
 })
 
-export const { setNews } = newsSlice.actions
+export const {setNews, setFilters} = newsSlice.actions
 
 export const newsReducer = newsSlice.reducer
