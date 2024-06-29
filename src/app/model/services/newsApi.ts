f8fcf825d3fc47385050ec8e58ea22d128ab02b1
Richard Leshik <richardleshik@gmail.com>
2024-06-29 23:42:07 +0300

    feat: update getNews in newsApi

--- src/app/model/services/newsApi.ts
+++ src/app/model/services/newsApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {TCategoriesApiResponse, TNewsApiResponse, TParams} from '../../../shared'
+import {setNews} from '../slices'
 
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
@@ -9,6 +10,7 @@ export const newsApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
 	endpoints: (builder) => ({
 		getNews: builder.query<TNewsApiResponse, TParams>({
+			keepUnusedDataFor: 0,
 			query: (params) => {
 				const {page_number = 1, page_size = 10, category, keywords} = params || {}
 
@@ -23,6 +25,12 @@ export const newsApi = createApi({
 					}
 				}
 			},
+			async onQueryStarted(_arg, {dispatch, queryFulfilled}) {
+				const result = await queryFulfilled
+				const data = result.data
+
+				dispatch(setNews(data.news))
+			}
 		}),
 		getLatestNews: builder.query<TNewsApiResponse, null>({
 			query: () => {
