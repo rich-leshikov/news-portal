f3476f561dae5b0cffa4a6cfd31310ac2448e3bb
Richard Leshik <richardleshik@gmail.com>
2024-07-05 21:50:57 +0300

    refactor: change withSkeleton.tsx props

--- src/shared/hocs/withSkeleton.tsx
+++ src/shared/hocs/withSkeleton.tsx
@@ -3,12 +3,14 @@ import {Skeleton} from '@/shared'
 import {TDirection, TSkeleton} from '../types'
 
 type Props = {
+	direction?: TDirection
 	isLoading: boolean
+	type?: TSkeleton
 }
 
-export const withSkeleton = <P extends object>(Component: ComponentType<P>, type?: TSkeleton, count?: number, direction?: TDirection) => {
+export const withSkeleton = <P extends object>(Component: ComponentType<P>, count?: number) => {
 	return (props: Props & P) => {
-		const {isLoading, ...restProps} = props
+		const {direction, isLoading, type, ...restProps} = props
 
 		if (isLoading) {
 			return <Skeleton type={type} count={count} direction={direction}/>
