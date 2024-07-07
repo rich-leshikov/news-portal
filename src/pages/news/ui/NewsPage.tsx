ffef4849a40cc6a4d409db02c4e99703c4a6bacb
Richard Leshik <richardleshik@gmail.com>
2024-07-07 16:23:44 +0300

    feat: update NewsPage.tsx

--- src/pages/news/ui/NewsPage.tsx
+++ src/pages/news/ui/NewsPage.tsx
@@ -1,11 +1,28 @@
 import {FC} from 'react'
+import {Link} from 'react-router-dom'
+import {useAppSelector} from '@/app'
+import {NewsDetails} from '@/entities'
 
 import styles from './styles.module.scss'
 
 export const NewsPage: FC = () => {
+	const currentNews = useAppSelector(state => state.news?.currentNews)
+
+	if (!currentNews) {
+		return (
+			<div>
+				<h1>Cannot find news</h1>
+				<Link to={'/'}>
+					<p>Go back</p>
+				</Link>
+			</div>
+		)
+	}
+
 	return (
 		<main className={styles.news}>
-			<h1>Header</h1>
+			<h1>{currentNews?.title}</h1>
+			<NewsDetails item={currentNews}/>
 		</main>
 	)
 }
